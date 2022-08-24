87ae054e26fae64487619abd1bb761c7c9833176
muhd-rashid <rashid.sulaiman93@gmail.com>
2022-08-24 12:38:15 +0800

    update UI and some events is custom DisplayAlert

--- Xperimen.Android/obj/Debug/110/android/environment.armeabi-v7a.s
+++ Xperimen.Android/obj/Debug/110/android/environment.armeabi-v7a.s
@@ -105,7 +105,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"6ca8aee9-1b57-4a2b-bef2-3d9198a7bb0a"
+	.asciz	"f96f27bc-3699-4fab-90e4-59b23fdf09b3"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
--- Xperimen/View/Login.xaml.cs
+++ Xperimen/View/Login.xaml.cs
@@ -30,18 +30,19 @@ namespace Xperimen.View
             if (string.IsNullOrEmpty(user))
             {
                 model.IsLoading = true;
-                SetDisplayAlert("Alert", "Please insert username", "Okay", "Not Okay");
+                SetDisplayAlert("Username empty !?", "Where is your username !? Please insert your username.", "Okay", "Dont Want");
                 entry_username.Isfocus = true;
             }
             else if (string.IsNullOrEmpty(password))
             {
                 model.IsLoading = true;
-                SetDisplayAlert("Alert", "Please insert password", "", "Wartaparg");
+                SetDisplayAlert("Password Empty !?", "Where is your password !? Please insert your password.", "Okay", "Dont Want");
                 entry_password.Isfocus = true;
             }
             else await Navigation.PushAsync(new MainPage());
         }
 
+        #region custom DisplayAlert implementation
         public void SetDisplayAlert(string title, string description, string btn1, string btn2)
         {
             alert.Title = title;
@@ -49,6 +50,60 @@ namespace Xperimen.View
             alert.TxtBtn1 = btn1;
             alert.TxtBtn2 = btn2;
             alert.IsVisible = true;
+
+            var tapClose = new TapGestureRecognizer();
+            tapClose.Tapped += CloseTapped;
+            var grid = (Grid)alert;
+            var btnClose = (Image)grid.Children[1];
+            btnClose.GestureRecognizers.Add(tapClose);
+
+            if (!string.IsNullOrEmpty(btn1))
+            {
+                var frame = (Frame)grid.Children[0];
+                var stack = (StackLayout)frame.Content;
+                var stack2 = (StackLayout)stack.Children[2];
+                var stackBtn = (StackLayout)stack2.Children[0];
+
+                var tapBtn1 = new TapGestureRecognizer();
+                tapBtn1.Tapped += Btn1Tapped;
+                stackBtn.GestureRecognizers.Add(tapBtn1);
+            }
+
+            if (!string.IsNullOrEmpty(btn2))
+            {
+                var frame = (Frame)grid.Children[0];
+                var stack = (StackLayout)frame.Content;
+                var stack2 = (StackLayout)stack.Children[2];
+                var stackBtn = (StackLayout)stack2.Children[2];
+
+                var tapBtn2 = new TapGestureRecognizer();
+                tapBtn2.Tapped += Btn2Tapped;
+                stackBtn.GestureRecognizers.Add(tapBtn2);
+            }
         }
+
+        public async void Btn1Tapped(object sender, EventArgs e)
+        {
+            var view = (StackLayout)sender;
+            await view.ScaleTo(0.9, 100);
+            view.Scale = 1;
+        }
+
+        public async void Btn2Tapped(object sender, EventArgs e)
+        {
+            var view = (StackLayout)sender;
+            await view.ScaleTo(0.9, 100);
+            view.Scale = 1;
+        }
+
+        public async void CloseTapped(object sender, EventArgs e)
+        {
+            var view = (Image)sender;
+            await view.ScaleTo(0.9, 100);
+            view.Scale = 1;
+            model.IsLoading = false;
+            alert.IsVisible = false;
+        }
+        #endregion
     }
 }
