97830121a461c5b464516e4869f3d236f36ef3b2
muhd-rashid <rashid.sulaiman93@gmail.com>
2022-08-29 19:12:02 +0800

    send parameter in XAML like cs file behind, test custom binding with UI

--- Xperimen/ViewModel/LoginViewmodel.cs
+++ Xperimen/ViewModel/LoginViewmodel.cs
@@ -1,11 +1,37 @@
 ﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xperimen.Helper;
 
 namespace Xperimen.ViewModel
 {
-    public class LoginViewmodel : BaseViewModel
+    public class LoginViewmodel : BaseViewModel, INotifyPropertyChanged
     {
+        string _purchaseRequisitions;
+        public string PurchaseRequisitions
+        {
+            get { return _purchaseRequisitions; }
+            set { _purchaseRequisitions = value; OnPropertyChanged(); }
+        }
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+        public new void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public ICommand StartTest { get; }
+
         public LoginViewmodel()
-        { }
+        {
+            StartTest = new Command(SetView);
+            PurchaseRequisitions = "First time assign text";
+        }
+
+        public async void SetView()
+        {
+            await Task.Delay(2000);
+            PurchaseRequisitions = "Second time assign text !!";
+        }
     }
 }
