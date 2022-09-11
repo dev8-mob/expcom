90fcb088e9574b1c0ecf818926152cb978f38099
muhd-rashid <rashid.sulaiman93@gmail.com>
2022-09-11 08:10:05 +0800

    bindablelayout.itemsource problem

--- Xperimen/ViewModel/Dashboard/DetailspageViewmodel.cs
+++ Xperimen/ViewModel/Dashboard/DetailspageViewmodel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xperimen.Model;
@@ -9,8 +8,8 @@ namespace Xperimen.ViewModel.Dashboard
 {
     public class DetailspageViewmodel : BaseViewModel
     {
-        public List<MobileApp> ListItems { get; }
-        public ICommand LoadDataCommand { get; }
+        public List<MobileApp> ListItems { get; set; }
+        public ICommand LoadDataCommand { get; set; }
 
         public DetailspageViewmodel()
         {
@@ -21,9 +20,9 @@ namespace Xperimen.ViewModel.Dashboard
 
         public void SetupView()
         {
-            IsLoading = true;
             try
             {
+                IsLoading = true;
                 ListItems.Add(new MobileApp { Id = Guid.NewGuid().ToString().Substring(0, 5), AppName = "Applab", AppSize = "35.4 MB", Description = "electrical plants is about 1,100,000 megawatts.The Sierra Club says the campaign has already prevented “100,792 asthma attacks, 9,420 heart attacks and 6,097 premature deaths” every year." });
                 ListItems.Add(new MobileApp { Id = Guid.NewGuid().ToString().Substring(0, 5), AppName = "Skytrex", AppSize = "15.9 MB", Description = "The White House says the president had hoped to pay more attention to a region with 1.3 billion potential customers for American goods, and a continent with bases that play a role in counterterrorism operations." });
                 ListItems.Add(new MobileApp { Id = Guid.NewGuid().ToString().Substring(0, 5), AppName = "Enginex", AppSize = "53.3 MB", Description = "To give her an opportunity to catch up, our daughter was placed in a class with one of the top teachers in the school and in a reduced sized classroom (16 kids)." });
