866ec2cda2d015cbd45caad509b8cb8784d7d25d
mraids <rashid.sulaiman93@gmail.com>
2022-10-01 14:14:54 +0800

    intialize custom calendar to be used in expenses module

---
+++ Xperimen/Stylekit/CustomCalendar.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xperimen.Stylekit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomCalendar : Grid
+    {
+        public DateTime CurrentDt;
+
+        public CustomCalendar()
+        {
+            InitializeComponent();
+            CurrentDt = DateTime.Now;
+            SetupView();
+        }
+
+        public void SetupView()
+        {
+
+        }
+    }
+}
