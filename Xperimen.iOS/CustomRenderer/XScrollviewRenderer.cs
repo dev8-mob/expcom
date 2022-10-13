33a7ca44b7002dae51cc16f528a1d35c89a52691
muhd-rashid <rashid.sulaiman93@gmail.com>
2022-10-13 12:08:41 +0800

    fix UI scrollview for ios got extra white space on top and bottom (only for ios 11 and above)

---
+++ Xperimen.iOS/CustomRenderer/XScrollviewRenderer.cs
@@ -0,0 +1,23 @@
+﻿
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Xperimen.iOS.CustomRenderer;
+using Xperimen.Stylekit;
+
+[assembly: ExportRenderer(typeof(XScrollview), typeof(XScrollviewRenderer))]
+namespace Xperimen.iOS.CustomRenderer
+{
+    public class XScrollviewRenderer : ScrollViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null) return;
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+                ContentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentBehavior.Never;
+        }
+    }
+}
---
+++ Xperimen/Stylekit/XScrollview.cs
@@ -0,0 +1,14 @@
+﻿
+using Xamarin.Forms;
+
+namespace Xperimen.Stylekit
+{
+    public class XScrollview : ScrollView
+    {
+        public XScrollview()
+        {
+            HorizontalOptions = LayoutOptions.FillAndExpand;
+            VerticalOptions = LayoutOptions.FillAndExpand;
+        }
+    }
+}
